d76ccbc4c75605b7ee548d85d5703f5f695c607b
superboi211 <70241841+superboi211@users.noreply.github.com>
2022-03-12 21:38:31 -0500

    Update ger.py

--- ger.py
+++ ger.py
@@ -5,28 +5,32 @@ import time
 args = sys.argv
 del args[0]
 x = 0
-program = open(args[0], 'r+').read().split('\n')
-lines = len(program)
-ger_var = None
-next_line = program[x + 1]
+if not args[0] == '-h':
+	program = open(args[0], 'r+').read().split('\n')
+	lines = len(program)
+	ger_var = None
+	next_line = program[x + 1]
 
-# A class used for making exception
-class WHAT(Exception):
-	pass
+	# A class used for making exception
+	class WHAT(Exception):
+		pass
 
-# Run commands
-while x < lines:
-	if program[x] == 'ger':
-		print(next_line)
-	elif program[x] == 'GER':
-		ger_var = input()
-	elif program[x] == 'Ger':
-		print(ger_var)
-	elif program[x] == 'gEr':
-		ger_var = next_line
-	elif program[x] == "geR":
-		time.sleep(int(next_line))
+	# Run commands
+	while x < lines:
+		if program[x] == 'ger':
+			print(next_line)
+		elif program[x] == 'GER':
+			ger_var = input()
+		elif program[x] == 'Ger':
+			print(ger_var)
+		elif program[x] == 'gEr':
+			ger_var = next_line
+		elif program[x] == "geR":
+			time.sleep(int(next_line))
+			x += 1
+		else:
+			raise WHAT('WHAT IS THAT (unknown instruction)??')
 		x += 1
-	else:
-		raise WHAT('WHAT IS THAT (unknown instruction)??')
-	x += 1
+
+else:
+	print('Usage: ger <file>')
