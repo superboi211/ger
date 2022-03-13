463cc49706249b472afdcacb0c7ac232f26a29ae
superboi211 <70241841+superboi211@users.noreply.github.com>
2022-03-12 20:54:08 -0500

    Update ger.py

--- ger.py
+++ ger.py
@@ -1,11 +1,14 @@
 #!/usr/local/bin/python3
 import sys
+import random
+import time
+
 args = sys.argv
 del args[0]
 x = 0
 program = open(args[0], 'r+').read().split('\n')
 lines = len(program)
-ger_var = 'A default string (you or the creator did something wrong)'
+ger_var = None
 next_line = program[x + 1]
 
 # A class used for making exception
@@ -22,6 +25,8 @@ while x < lines:
 		print(ger_var)
 	elif program[x] == 'gEr':
 		ger_var = next_line
+	elif program[x] == "geR":
+		time.sleep(next_line)
 	else:
 		raise WHAT('WHAT IS THAT (unknown instruction)??')
 	x += 1
