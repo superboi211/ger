fe55f46d813bcd5074e3eeb58e887d96c5cadab8
superboi211 <70241841+superboi211@users.noreply.github.com>
2022-03-13 08:02:45 -0400

    because yes

---
+++ ger.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/python3
+import sys
+import time
+
+args = sys.argv
+del args[0]
+x = 0
+if not args[0] == '-h':
+	program = open(args[0], 'r+').read().split('\n')
+	lines = len(program)
+	ger_var = None
+	next_line = program[x + 1]
+
+	# A class used for making exception
+	class WHAT(Exception):
+		pass
+
+	# Run commands
+	while x < lines:
+		if program[x] == 'ger':
+			print(next_line)
+		elif program[x] == 'GER':
+			ger_var = input()
+		elif program[x] == 'Ger':
+			print(ger_var)
+		elif program[x] == 'gEr':
+			ger_var = next_line
+		elif program[x] == "geR":
+			time.sleep(int(next_line))
+			x += 1
+		else:
+			raise WHAT(f'GER: SYNTAX ERROR, UNKNOWN INSTRUCTION: {program[x]} AT LINE {x}!!!')
+		x += 1
+
+else:
+	print('Usage: ger <file>')
+
+	print('Version: 1.0.3')
