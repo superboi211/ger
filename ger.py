7a6db4f876114681107d4efe649c076cccca0747
CringeYt <61865847+jitty-code@users.noreply.github.com>
2022-03-24 12:33:08 +0500

    Cleaned up code, much better
    I finally did it

--- ger.py
+++ ger.py
@@ -1,4 +1,7 @@
 #!/usr/local/bin/python3
+from ast import arg
+from inspect import signature
+import string
 import sys
 import time
 import re
@@ -7,17 +10,42 @@ args = sys.argv
 del args[0]
 x = 0
 
+if len(args) == 0 or args[0] == '--help':
+    print('Usage: python3 ger.py <file>')
+    print('Version: 1.6.0')
+    exit()
+
 program = open(args[0], 'r+').read().split('\n')
 lines = len(program)
 ger_stack = []
 next_line = program[x + 1]
 
+def err(error):
+    print(f'GER: {error}')
+    exit()
+
 instructions = {}
-
-
 def instruction(func):
-    instructions[func.__name__] = func
-    return func
+    def inner():
+        global x
+        argcount = len(signature(func).parameters)
+        args = []
+        argnum = 1
+
+        while argnum <= argcount:
+            try:
+                args.append(program[x + argnum])
+            except IndexError:
+                err(f'UNEXPECTED END OF FILE FOR INSTRUCTION {program[x]}')
+
+            argnum += 1
+
+        x += argcount
+        func(*args)
+    
+
+    instructions[func.__name__] = inner
+    return inner
 
 
 ########################
@@ -25,36 +53,27 @@ def instruction(func):
 # WHERE MOST CODE GOES #
 ########################
 @instruction
-def ger():
+def ger(text):
     global x
 
-    try:
-        str_array = re.split('; ', program[x + 1])
-    except IndexError:
-        str_array = ['', '']
-
-    try:
-        if str_array[1] == 'insert_newline':
-            print('\n', end='')
-        elif str_array[1] == 'insert_space':
-            print(' ', end='')
-        else:
-            print(str_array[1], end='')
-        x += 1
-    except:
-        err(f'UNEXPECTED END OF FILE!!! FOR {program[x]} AT LINE {x + 1}!!!')
+    if text.startswith("; "):
+        print(text.replace("; ", ""))
+    elif text == 'insert_newline':
+        print('\n', end='')
+    elif text == 'insert_space':
+        print(' ', end='')
+    else:
+        err('INVALID INPUT OF "ger". Did you forget to put "; " at the start?')
 
 
 @instruction
 def GER():
     ger_push(str(input()))
 
-
 @instruction
 def Ger():
     print(ger_stack[len(ger_stack) - 1], end='')
     ger_pop()
-
 
 @instruction
 def gEr():
@@ -63,14 +82,12 @@ def gEr():
         x += 1
     except:
         # End of file when something else needed
-        err(f'UNEXPECTED END OF FILE!!! FOR {program[x]} AT LINE {x + 1}!!!')
-
+        err(f'UNEXPECTED END OF FILE!!! {program[x]} AT LINE {x + 1}!!!')
 
 @instruction
 def geR():
     time.sleep(int(next_line))
     x += 1
-
 
 @instruction
 def ge_r():
@@ -85,41 +102,22 @@ def ge_r():
 # WHERE MOST CODE GOES #
 ########################
 
-
-if len(args) == 0 or args[0] == '--help':
-    print('Usage: python3 ger.py <file>')
-    print('Version: 2.0.0')
-    exit()
-
-# A class used for making exception
-
-
-def err(error):
-    print(f'GER: {error}')
-    exit()
-
-
 def ger_push(what_push: str):
     ger_stack.append(what_push)
-
 
 def ger_pop():
     ger_stack.pop()
 
-
 def runCode():
-    ignore = program[x].startswith(
-        'COMMENT: ') or program[x].replace(' ', '') == ''
+    ignore = program[x].startswith('COMMENT: ') or program[x].replace(' ', '') == ''
 
-    if (ignore):
-        return
-    if program[x] not in instructions.keys():
+    if (ignore): return
+    if (not program[x] in instructions.keys()):
         err(
             f'SYNTAX ERROR: WHAT IS "{program[x]}"??? THE ERROR IS AT LINE {x + 1}!!!'
         )
     instructions[program[x]]()
 
-
-while x < lines:
+while x < lines: 
     runCode()
     x += 1
